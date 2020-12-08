6d51fced8f1b06cb8e47451025f8cf0c3b551441
Alexandros <lexbarlas@gmail.com>
2020-12-08 23:44:49 +0200

    ughhhh

--- src/ArrayExercises/ArrayIntersection.java
+++ src/ArrayExercises/ArrayIntersection.java
@@ -2,28 +2,56 @@ package ArrayExercises;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 
 public class ArrayIntersection {
   public static int[] intersect(int[] nums1, int[] nums2) {
-    boolean smallerOne = nums1.length < nums2.length;
-    int smallerArray = smallerOne? nums1.length : nums2.length;
-    int[] smaller = smallerOne ? nums1 : nums2;
-    int largerArray = smallerOne? nums2.length : nums1.length;
-    int[] larger = smallerOne? nums2 : nums1;
-    ArrayList<Integer> intersection = new ArrayList<>();
+    if (nums1.length == 0 || nums2.length == 0) return new int[]{};
+    ArrayList<Integer> sorted1 = new ArrayList<>();
+    Arrays.stream(nums1).boxed().forEach(x -> sorted1.add(x));
+    Collections.sort(sorted1);
+    ArrayList<Integer> sorted2 = new ArrayList<>();
+    Arrays.stream(nums2).boxed().forEach(x -> sorted2.add(x));
+    Collections.sort(sorted2);
+    HashMap<Integer, Integer> counts = new HashMap<>();
 
-    for (int i = 0; i < largerArray;){
-      boolean notAdded = true;
-      for (int j = 0; j < smallerArray; j++){
-        if (smaller[j] == larger[i] && notAdded) {
-          intersection.add(smaller[j]);
-          notAdded = false;
+    int i = 0,j = 0;
+    while (sorted1.get(i) > sorted2.get(j)) {
+      j++;
+    }
+    while (sorted1.get(i) < sorted2.get(j)) {
+      i++;
+    }
+    System.out.println(i);
+    System.out.println(j);
+    while (i < sorted1.size()) {
+      Integer one = sorted1.get(i);
+      int nj = j;
+      while(nj < sorted2.size()) {
+        Integer two = sorted2.get(nj);
+        if (one == two) {
+          if (counts.containsKey(one)) counts.put(one, counts.get(one) + 1);
+          else counts.put(one, 1);
+          break;
         }
-        i++;
+        nj++;
       }
       i++;
     }
-    return intersection.stream().mapToInt(i -> i).toArray();
+    int sum = 0;
+    for (Integer val : counts.values()) {
+      sum += val;
+    }
+    int[] intersection = new int[sum];
+    int k = 0;
+    for (Integer o : counts.keySet()) {
+      for (int l = 0; l < counts.get(o); l++) {
+        intersection[k] = o;
+        k++;
+      }
+    }
+    return intersection;
   }
 
   public static void main(String[] args) {
